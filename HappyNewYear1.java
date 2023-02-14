0a7f591045ae639f0f59e68df3176daf6527fe2b
JRAKASH <58143158+JRAKASH@users.noreply.github.com>
2023-02-14 09:40:10 +0530

    Create HappyNewYear1.java
    pattern 1

---
+++ HappyNewYear1.java
@@ -0,0 +1,20 @@
+package pattern1.print1;
+
+public class HappyNewYear1 {
+
+	public static void main(String[] args) {
+		System.out.println("Happy *** *New*** Year");
+		for(int i=0;i<=4;i++)
+		{		
+			for(int j=0;j<=i;j++) {
+		System.out.print(" "+2+" ");
+		System.out.print(" "+0+"  ");
+		System.out.print(" "+2+"  ");
+		System.out.print(" "+3+"  ");
+		}
+			System.out.println();
+		}
+		
+	}
+
+}
